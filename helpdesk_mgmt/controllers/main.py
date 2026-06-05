a84994ef604ec38c9adc05a9b947bd6271b29eb6
Víctor Martínez <victor.martinez@tecnativa.com>
2026-06-05 16:39:21 +0200

    [FIX] helpdesk_mgmt: Display the appropriate categories on the portal
    TT62933

--- helpdesk_mgmt/controllers/main.py
+++ helpdesk_mgmt/controllers/main.py
@@ -43,7 +43,8 @@ class HelpdeskTicketController(http.Controller):
     def _get_categories(self, **kw):
         company = request.env.company
         category_model = http.request.env["helpdesk.ticket.category"]
-        return category_model.with_company(company.id).search([("active", "=", True)])
+        domain = [("active", "=", True), ("show_in_portal", "=", True)]
+        return category_model.with_company(company.id).search(domain)
 
     @http.route("/new/ticket", type="http", auth="user", website=True)
     def create_new_ticket(self, **kw):
